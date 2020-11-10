43df7971a797b42371bf873480fda6e3b652e6a5
Jordan Bradley <jordanrhysbradley@gmail.com>
2020-11-10 14:57:43 +1000

    route test for heroku

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -1,5 +1,7 @@
 class Vinyl < ApplicationRecord
-    attribute :price, default: 0.5
+    attribute :price, default: 1.0
+
+    validates_presence_of :title, :artist, :label, :genre, :year, :quality, :price
 
     has_one_attached :image
 end
