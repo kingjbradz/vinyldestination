854226d9e371fba142066f1d34857373eef938ad
Jordan Bradley <jordanrhysbradley@gmail.com>
2020-11-10 17:34:44 +1000

    fixing syntactic error on route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :vinyls
   devise_for :users
-  root "home#page"
+  root to: "home#page"
 
   get "/contact", to: "contact#contact", as: "contact"
 
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
