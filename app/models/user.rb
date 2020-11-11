5be8293747ca1d8d2478500c5e003fb7f566f0bf
Jordan Bradley <jordanrhysbradley@gmail.com>
2020-11-11 18:30:34 +1000

    added username function, alphanumerical requirement for username, unique requirement as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }, uniqueness: true
+
+  validates_presence_of :username
+
   has_many :vinyls
 end
