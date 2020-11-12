2cdc5ab76f7b4281e6e1495cda04f513425c9153
Jordan Bradley <jordanrhysbradley@gmail.com>
2020-11-12 15:00:52 +1000

    removed 'Account' from navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :vinyls
+
   devise_for :users
 
   root to: "home#page"
-  
+
   get 'listing/page', to: "listing#page"
 
   get "/contact", to: "contact#contact", as: "contact"
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
